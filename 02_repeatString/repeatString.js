ec64888f1874894e725138f664005cb99bae721e
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-05 17:34:47 -0400

    created function that takes in a string and a number and returns a new string with the original string repeated the given number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function(string) {
-    return string * 3
-};
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newString = newString + string;
+        }
+        return newString
+    } else {
+        return "ERROR"
+    };
+}
 
 // Do not edit below this line
 module.exports = repeatString;
