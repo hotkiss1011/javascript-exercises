19a90596b04ae46af83eb6509600f05f39987496
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-07 19:13:37 -0400

    completed sumAll function and passed all tests. function will give an ERROR if a negative number or non-integer is given, but give the sum of all numbers starting from the lowest number given to the highest.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let low = 0;
+    let high = 0;
+    
+    if (num1 > 0 && num2 > 0 && Number.isInteger(num1) && Number.isInteger(num2)) {
+        if (num1 > num2) {
+            low = num2;
+            high = num1;
+        } else {
+            low = num1;
+            high = num2;
+        };
+        
+        for (let i = low; i <= high; i++) {
+            sum += i;
+        };
+        return sum;
+    } else {
+        return "ERROR";
+    };
 };
 
 // Do not edit below this line
