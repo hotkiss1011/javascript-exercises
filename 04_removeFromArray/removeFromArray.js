b33587472b7003f6a365ef96320281f0eccf44b7
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-07 16:57:48 -0400

    completed remove from array challenge and passed all tests. function creates and returns a new array with all numbered arguments removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = array.filter(function(arr) {return args.indexOf(arr) == -1});
+            
+    return newArray;
 };
 
 // Do not edit below this line
