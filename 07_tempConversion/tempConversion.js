67715163d5bd3b22e392431631c4a1e5653bee21
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-08 12:05:41 -0400

    completed challenge 7 tempConversion functions and passed all tests. functions takes in a number and returns the number converted into either celsius(ftoc) or fahrenheit(ctof)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let c = (temp - 32) * 5 / 9;
+  c = Math.round(c * 10) / 10;
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let f = (temp * 9 / 5) + 32;
+  f = Math.round(f * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
