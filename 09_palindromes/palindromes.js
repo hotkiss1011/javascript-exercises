37658264f49762fee0a7a9603b97ba503c60fc77
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-20 17:20:27 -0400

    completed palindrom exercise and passed tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //clean up string: remove special characters and make lowercase
+    let cleanString = string.replace(/[^a-zA-Z]/g, "");
+    cleanString = cleanString.toLowerCase();
+    //make another string that is the given string spelled backwards
+    let backwards = cleanString.split("").reverse().join('')
+    //check if backwards string is the same as the given string and return true/false
+    return cleanString == backwards;
 };
 
 // Do not edit below this line
