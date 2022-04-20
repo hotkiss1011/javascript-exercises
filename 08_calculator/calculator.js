067e2463512b4a8d662fd39a2ab3fcafa5d7346d
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-20 09:10:12 -0400

    completed calculator and passed tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumAll = 0;
+	for (let i = 0; i < array.length; i++) {
+    sumAll += array[i];
+  }
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplied = 1;
+	for (let i = 0; i < array.length; i++) {
+    multiplied *= array[i]
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
