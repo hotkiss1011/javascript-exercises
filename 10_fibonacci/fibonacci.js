d9485d1095fb0166530830ee9b0ea5403897a4de
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-21 19:54:35 -0400

    completed fibonacci and passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let formattedNum = Number(num);
+    let array = [0, 1];
+    if (formattedNum > 0) {
+        for (let i = 2; i <= formattedNum; i++) {
+            array[i] = array[i-1] + array[i - 2];
+        }
+        return array[formattedNum];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
