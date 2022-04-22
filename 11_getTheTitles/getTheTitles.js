0a6cd25e861672f671f66745f527f0016d53dea2
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-21 20:31:21 -0400

    completed getTheTitles and passed tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArray = [];
+    array.forEach(e => {
+        newArray.push(e.title);
+    });
+    return newArray;
 };
 
 // Do not edit below this line
