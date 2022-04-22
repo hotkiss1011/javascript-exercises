c254c388ac013a771349babf64074133e9ca31b8
amanda hotchkiss <amanda.hotchkiss1011@gmail.com>
2022-04-21 21:52:11 -0400

    completed findTheOldest and passed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let death = 0;
+    let ages = [];
+    array.forEach(e => {
+        if (e.yearOfDeath) {
+            death = e.yearOfDeath;
+        } else {
+            today = new Date();
+            death = today.getFullYear();
+        }
+        ages.push(death - e.yearOfBirth);
+    });
 
+    let oldest = ages.indexOf(Math.max(...ages));
+    return array[oldest];
 };
 
 // Do not edit below this line
